50dd4dc01a8f241863ed34b8310f242eb331ea31
Sergei Vizel <sergei.vizel@modera.net>
2014-01-24 15:29:57 +0200

    ScriptHandler::clearCache

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -4,6 +4,8 @@ namespace Modera\Module\Composer;
 
 use Composer\Composer;
 use Composer\Script\CommandEvent;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
 use Modera\Module\Service\ComposerService;
 
 /**
@@ -39,5 +41,75 @@ class ScriptHandler
         if (file_exists($file)) {
             file_put_contents($file, implode("\n", $data) . "\n");
         }
+    }
+
+    /**
+     * Clears the Symfony cache.
+     *
+     * @param $event CommandEvent A instance
+     */
+    public static function clearCache(CommandEvent $event)
+    {
+        $options = self::getOptions($event);
+        $appDir = $options['symfony-app-dir'];
+
+        if (!is_dir($appDir)) {
+            echo 'The symfony-app-dir ('.$appDir.') specified in composer.json was not found in '.getcwd().', can not clear the cache.'.PHP_EOL;
+
+            return;
+        }
+
+        static::executeCommand($event, $appDir, 'cache:clear --env=prod --no-warmup', $options['process-timeout']);
+    }
+
+    /**
+     * @param CommandEvent $event
+     * @param $appDir
+     * @param $cmd
+     * @param int $timeout
+     * @throws \RuntimeException
+     */
+    protected static function executeCommand(CommandEvent $event, $appDir, $cmd, $timeout = 300)
+    {
+        $php = escapeshellarg(self::getPhp());
+        $console = escapeshellarg($appDir . '/console');
+        if ($event->getIO()->isDecorated()) {
+            $console .= ' --ansi';
+        }
+
+        $process = new Process($php.' '.$console.' '.$cmd, null, null, null, $timeout);
+        $process->run(function ($type, $buffer) { echo $buffer; });
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException(sprintf('An error occurred when executing the "%s" command.', escapeshellarg($cmd)));
+        }
+    }
+
+    /**
+     * @param CommandEvent $event
+     * @return array
+     */
+    protected static function getOptions(CommandEvent $event)
+    {
+        $options = array_merge(array(
+            'symfony-app-dir' => 'app',
+        ), $event->getComposer()->getPackage()->getExtra());
+
+        $options['process-timeout'] = $event->getComposer()->getConfig()->get('process-timeout');
+
+        return $options;
+    }
+
+    /**
+     * @return false|string
+     * @throws \RuntimeException
+     */
+    protected static function getPhp()
+    {
+        $phpFinder = new PhpExecutableFinder;
+        if (!$phpPath = $phpFinder->find()) {
+            throw new \RuntimeException('The php executable could not be found, add it to your PATH environment variable and try again');
+        }
+
+        return $phpPath;
     }
 }
